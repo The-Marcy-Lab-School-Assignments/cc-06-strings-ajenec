282c4b6af861b71fbaef2310c12286f564dc610d
Ajene <ajenec@gmail.com>
2024-10-10 11:02:40 -0400

    not done yet

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+
+//1
+const reverseString = (str) => {
+  let output = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    output += str[i];
+  }
+  return output;
+};
+
+console.log(reverseString("hello"));
+
+//2
+const reverseZigZagString = (str) => {
+  let result = " ";
+  for (let i = str.length - 1; i >= 0; i--) {}
+};
