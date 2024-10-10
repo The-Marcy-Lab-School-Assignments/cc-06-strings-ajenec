545e7377f2127306f70a3fa55ce91ab1ac4e6e85
Ajene <ajenec@gmail.com>
2024-10-10 11:12:13 -0400

    done

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ const reverseString = (str) => {
   return output;
 };
 
-console.log(reverseString("hello"));
+//console.log(reverseString("hello"));
 
 //2
 const reverseZigZagString = (str) => {
+  let isSmall = true;
   let result = " ";
-  for (let i = str.length - 1; i >= 0; i--) {}
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (isSmall) {
+      result += str[i].toLowerCase();
+      isSmall = false;
+    } else {
+      result += str[i].toUpperCase();
+      isSmall = true;
+    }
+  }
+  return result;
 };
+console.log(reverseZigZagString("hello"));
